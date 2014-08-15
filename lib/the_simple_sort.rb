6d3d11514c217a555d327b96e31052e9413627f2
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-08-15 08:19:10 +0400

    ~~~

--- lib/the_simple_sort.rb
+++ lib/the_simple_sort.rb
@@ -7,8 +7,8 @@ module TheSimpleSort
     extend ActiveSupport::Concern
 
     included do
-      scope :recent, ->(field = :id) { reorder("#{ field } DESC") if field }
-      scope :old,    ->(field = :id) { reorder("#{ field } ASC")  if field }
+      scope :recent, ->(field = :id) { reorder("#{ field } DESC") if field && self.columns.map(&:name).include?(field.to_s) }
+      scope :old,    ->(field = :id) { reorder("#{ field } ASC")  if field && self.columns.map(&:name).include?(field.to_s) }
 
       scope :simple_sort, ->(params, default_sort_field = nil){
         sort_column = params[:sort_column]
